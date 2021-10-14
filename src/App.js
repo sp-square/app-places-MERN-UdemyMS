7b24d5a1199a9489d5bab89ba012167bec27e0f4
Sandrine Poissonnet <spoisson@gmail.com>
2021-10-14 14:06:42 -0700

    adding a modal

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+	BrowserRouter as Router,
+	Route,
+	Redirect,
+	Switch,
+} from 'react-router-dom';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+import Users from './users/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
+
+const App = () => {
+	return (
+		<Router>
+			<MainNavigation />
+			<main>
+				<Switch>
+					<Route exact path="/">
+						<Users />
+					</Route>
+					<Route exact path="/:userId/places">
+						<UserPlaces />
+					</Route>
+					<Route exact path="/places/new">
+						<NewPlace />
+					</Route>
+					<Redirect to="/" />
+				</Switch>
+			</main>
+		</Router>
+	);
+};
+
+export default App;
